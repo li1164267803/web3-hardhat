b00bd294d9daa2dd3b64120a29b57d73dea0c37a
李琳 <xiwenpp@163.com>
2024-10-28 22:21:34 +0800

    first commit

---
+++ contracts/Counter.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+contract Counter {
+    string public name = "asdad";
+    uint public age = 20;
+
+    function adad() public view returns (string memory) {
+        return name;
+    }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require('@nomicfoundation/hardhat-toolbox')
+
+// 公钥
+// 0xa8D2C6Bb8567FE064059C7FE708D739c43Ba4028
+// 私钥
+// 0x32ffe114bd96e680ac4ea4f68a6f1b00b3df863522cf954a94798212b3738225
+// 助记词
+// cement bridge feed mountain brush craft swamp resist disorder spirit bitter element
+// 推导路径
+// m/44'/60'/0'/0/0
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: '0.8.27',
+    networks: {},
+}
